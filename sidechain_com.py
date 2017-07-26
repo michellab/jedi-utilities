904a72b2d685c502ab85aecfa2d74eb5d183eaee
Joan Clark-Nicolas <joan.clark88@gmail.com>
2017-07-26 14:18:14 +0100

    now clusters the snapshots and  the reference structures to bias away from. Code needs to be cleaned up. A function to generate automatically the new plumed.dat file needs to be added

--- sidechain_com.py
+++ sidechain_com.py
@@ -1,4 +1,4 @@
-import argparse,numpy,os,scipy
+import argparse,numpy,os,mdtraj
 
 parser = argparse.ArgumentParser(description="Takes a protein binding site and generates a \
                                               PLUMED input to calculate the COM of each side\
@@ -12,7 +12,7 @@ parser.add_argument('-a','--apolar', nargs="?",
                     default='apolar.pdb')
 parser.add_argument('-p','--polar', nargs="?",
                     help='Name of the pdb file containing the polar atoms of the binding site',
-                    default='apolar.pdb')
+                    default='polar.pdb')
 parser.add_argument('-o','--output', nargs="?",
                     help='Name of the input file to supply to PLUMED',
                     default='com.dat')
@@ -21,11 +21,13 @@ parser.add_argument('-s','--stride', nargs="?",
                     default='1')
 parser.add_argument('-t','--trr', nargs="?",
                     help='gromacs trr trajectory to process with PLUMED driver')
+parser.add_argument('-g','--gro', nargs="?",
+                    help='Name of input GRO file.')
 
 
 def parse(parser):
     args = parser.parse_args()
-    return args.apolar,args.polar,args.input,args.output,args.stride,args.trr
+    return args.apolar,args.polar,args.input,args.output,args.stride,args.trr,args.gro
 
 
 
@@ -112,6 +114,7 @@ def Clustering(labels,time,values): # Done as in Rodriguez & Laio, Science(2014)
     
    #calculate euclidean distances:
     euclidMat=[]
+    euclidMat_avg=[]
     for i in range (0,len(values)):
         euclid=[]
         for j in range(0,len(values)):
@@ -119,15 +122,16 @@ def Clustering(labels,time,values): # Done as in Rodriguez & Laio, Science(2014)
                euclid.append(99999999.)
             elif j>i:
                euclid.append(numpy.linalg.norm(values[i]-values[j]))
+               euclidMat_avg.append(numpy.linalg.norm(values[i]-values[j]))
             elif j<i:
                euclid.append(99999999.)
         euclidMat.append(euclid)
     euclidMat=numpy.array(euclidMat)
     #print euclidMat
-         
+    euclidMat_avg=numpy.mean(euclidMat_avg)
     
     #calculte rho for each data point
-    d0=2. #This value needs to be calculated somehow!!!
+    d0=euclidMat_avg/2 #This parameter needs to be optimised for each system and CV  
     rho=[]
     for i in range(0,len(values)):
         rhoi=0.
@@ -159,7 +163,10 @@ def Clustering(labels,time,values): # Done as in Rodriguez & Laio, Science(2014)
                   deltai=euclidMat[j][i]
         delta.append(deltai)
     
-    # correct maximum element. This is done different than in the paper!!!!!
+    # correct the value for the point with maximum density. 
+    #This is done different than in the paper. In the paper
+    # the delta value for this point is the distance with
+    # the furthest point.
     deltai=0.
     for i in range(0,len(delta)):
         if delta[i]!=999999999. and delta[i]>deltai:
@@ -174,11 +181,108 @@ def Clustering(labels,time,values): # Done as in Rodriguez & Laio, Science(2014)
         fileout.write(line)
     fileout.close()
 
-    print max(delta)
+
+    # Select number of clusters by visual inspection and assign centers
+    Nclust=int(raw_input("Plot the rhodelta file and tell me how many cluster centers you observe:\n"))
+   
+    deltaCenters=sorted(delta,reverse=True)[0:Nclust]
+
+    clusterCenters=[]
+    for i in range(0,len(values)):
+        if delta[i] in deltaCenters:
+           clusterCenters.append(i)
+    print "Found",len(clusterCenters), "cluster centers"
+    
+    clusters={}
+    for center in clusterCenters:
+        print "Found center", center, "at time=", time[center], "picoseconds."
+        clusters[time[center]]=[]
+    
+    # Assign each point to the cluster corresponding to the closest cluster center
+    for i in range(0,len(values)):
+        disti=[]
+        for j in clusterCenters:
+            if j==i:
+                disti.append(0.)
+            elif j>i:
+               disti.append(euclidMat[i][j])
+            elif j<i:
+               disti.append(euclidMat[j][i])
+        mindisti=min(disti)
+        jmindist=disti.index(min(disti))
+        center=clusterCenters[jmindist]
+        clusters[time[center]].append(time[i])
+    
+    #check how many elements are in each cluster
+    totalElements=0
+    for center in clusters.keys():
+        print "cluster with center at ", center, "picoseconds has ", len(clusters[center]), "elements"
+        totalElements=totalElements+len(clusters[center])
+    print "Total elements clustered:", totalElements, ". Number of observations: ",len(values)," (MUST BE THE SAME)"
+   
+    return clusters
+
+def genRefs(gro,trr,apolar,polar,clusters):
+
+    name=trr.split('.')[0]
+
+    traj=mdtraj.load_trr(trr, top=gro)
+    time=traj.time.tolist()
+
+    #select atoms in the bsite
+    reflist=[]
+    for pdb in [apolar,polar]:
+       filein=open(pdb,'r')
+       for line in filein:
+           if line.startswith('ATOM') or line.startswith('HETATM'):
+              reflist.append(int(line.split()[1])-1)
+       filein.close()
+    reflist=sorted(reflist)
+        
+        
+    for key in clusters.keys():
+        outfile='ref_'+name+'_'+str(int(key))+'.pdb'
+        framenumber=time.index(key)
+        coords=traj.xyz[framenumber]
+        top=traj.topology
+        frame=mdtraj.Trajectory(coords,top)
+        ref=frame.atom_slice(reflist)
+        ref.save_pdb('temp.pdb')
+    #    sys.exit()
+        filein=open('temp.pdb','r')
+        fileout=open(outfile,'w')
+        idx=0
+        for line in filein:
+            if line.startswith('ATOM') or line.startswith('HETATM'):
+               pdb_idx=reflist[idx]+1
+               mass = frame.topology.atom(reflist[idx]).element.mass
+               if pdb_idx < 10:
+                  str_idx = '    %s' % pdb_idx
+               elif pdb_idx < 100:
+                  str_idx= '   %s' % pdb_idx
+               elif pdb_idx < 1000:
+                  str_idx = '  %s' % pdb_idx
+               elif pdb_idx < 10000:
+                  str_idx = ' %s' % pdb_idx
+               else:
+                  str_idx = '%s' % pdb_idx
+
+               str_mass = '%-5.2f' % mass
+
+               line = line[0:6] + str_idx + line[11:55] + str_mass + line[60:61] + '1.00' + line[66:] # modifying b-factor so the rmsd is properly calculated
+               idx=idx+1
+            fileout.write(line)
+        filein.close()
+        fileout.close()     
+        os.system('rm temp.pdb')
+            
+
+
+
 
 if __name__=='__main__':
     
-    apolar,polar,structure,output,stride,trr = parse(parser)
+    apolar,polar,structure,output,stride,trr,gro = parse(parser)
     
     residues = findResidues(apolar,polar)
     
@@ -188,5 +292,8 @@ if __name__=='__main__':
 
     labels,time,values=Plumed(trr,output,stride)
 
-    Clustering(labels,time,values)
+    clusters=Clustering(labels,time,values)
+    
+    genRefs(gro,trr,apolar,polar,clusters)
+    
     
