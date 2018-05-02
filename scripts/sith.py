30c2d54c73216ca4018b6adf0eab117d46393f32
carajillu <joan.clark88@gmail.com>
2018-05-02 15:23:35 +0100

    Started the function to bias with SC_RMSD and also to calculate the maximum at_metric when using TORSIONs

--- scripts/sith.py
+++ scripts/sith.py
@@ -1095,7 +1095,6 @@ def build_metric_bias(parameters,clusters,metric_arr,iteration,metric_input,time
        if clusdist_avg=="max" and 'TORSION' in parameters['metric']: 
           max_dist=2*math.sqrt(len(metric_input.keys()))
           print max_dist
-          sys.exit()
 
        dt=float(parameters['dt'])
        stride=int(parameters['stride'])
