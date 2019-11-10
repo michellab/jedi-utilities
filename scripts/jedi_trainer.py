737552670e27889cf26652bd5778807f8dd1ddb3
carajillu <joan.clark88@gmail.com>
2019-11-10 11:20:39 +0000

    Now Finished the bits that calculate CCmin+deltaCC and CC2min+deltaCC2. Need to modify the one that selects gridpoints close to ligand atoms so that it only selects gridpoints that are close to ligand atoms that in turn are close to protein atoms (otherwise exposure will make no sense). Then need to check how good exposures are for those. Good job!

--- scripts/jedi_trainer.py
+++ scripts/jedi_trainer.py
@@ -4,6 +4,8 @@ import numpy as np
 import pandas as pd
 import scipy as scp
 from scipy import stats
+import matplotlib
+import matplotlib.pyplot as plt
     
 def parser():
     """
@@ -20,12 +22,14 @@ def parser():
                               Must be in a directory that contains a subdirectory for each PDB.')
     parser.add_argument('-d','--druggability', nargs="?",
                     help='Name of the value used as druggability measure. Must be present in --input as a column')
-    parser.add_argument('-dt','--d_theta', nargs="?", default=0.1,
+    parser.add_argument('-dt','--d_theta', nargs="?", default=0.1, type=float,
                     help='increment for theta optimisation')
-    parser.add_argument('-tmi','--theta_max_iter', nargs="?", default=100,
+    parser.add_argument('-tmi','--theta_max_iter', nargs="?", default=100, type=int,
                     help='Maximum number of iterations for the optimisation of theta')
-    parser.add_argument('-tt','--theta_tolerance', nargs="?", default=0.0001,
+    parser.add_argument('-tt','--theta_tolerance', nargs="?", default=0.0001, type=float,
                     help='Maximum number of iterations for the optimisation of theta')
+    parser.add_argument('-lmd','--lig_mindist', nargs="?", default=0.2, type=float,
+                    help='maximum distance gridpoint-ligand to consider them overlapping')
     args=parser.parse_args()
 
     return args
@@ -128,17 +132,130 @@ def theta_trainer(dataset,r2_target,tolerance,max_iter,d_theta):
     
     return theta_selected
 
-def cc_min_trainer(dataset, theta):
-    """
-    Input: protein, grid, ligand, all as MDTraj objects
-    """
+def get_points_ligand(dataset, lig_mindist):
+    dataset_points_ligand=[]
+    for i in range(0,len(dataset['PDB'])):
+        ligand=dataset['ligand'][i].xyz[0]
+        grid=dataset['grid'][i].xyz[0]
+        points_ligand=[]
+        for j in range(0,len(grid)):
+            r_min=get_r_min(grid[j],ligand)
+            if r_min<=lig_mindist:
+               points_ligand.append(j)
+        dataset_points_ligand.append(points_ligand)
+    dataset['points_ligand']=dataset_points_ligand
+    return dataset
+
+def cc_min_trainer(dataset, theta, percentage):
+    mindist_active=[]
+    for i in range(0,len(dataset['PDB'])):
+        protein=dataset['protein'][i].xyz[0]
+        grid=dataset['grid'][i].xyz[0]
+        for j in range(0,len(grid)):
+            if (j not in dataset['points_ligand'][i]):
+                continue
+            mindist=calc_mindist(grid[j],protein,theta) # This is used in the calculation of JEDI, so need to use Eq 6 from paper
+            if (mindist<0.15):
+                print(dataset['PDB'][i], j)
+            mindist_active.append(mindist)
+    mindist_active.sort(reverse=True) # This is a Soff, so a bigger value will mean less active points
+    print("------------ CCmin+deltaCC --------------")
+    for pcent in [0.2,0.4,0.6,0.8,1.0]:
+        i=int(len(mindist_active)*pcent)-1
+        cc_i=mindist_active[i]
+        print(pcent, cc_i)
+    pcent=percentage/100
+    i=int(len(mindist_active)*pcent)-1
+    cc=mindist_active[i]
+    print("CCmin+deltaCC value that covers ", percentage, "\% of points overlapping with a ligand atom: ", cc)
+    print("------------ CCmin+deltaCC --------------")
+
+    #Plot a histogram for Thesis / paper purposes
+    fig=plt.figure(figsize=(20,10))
+    plt.hist(mindist_active, density=True, stacked=True, bins = 10)
+    plt.xlabel("Minimum distance grid point - protein atom (nm)",fontsize=20)
+    plt.ylabel("Probability", fontsize=20)
+    plt.savefig("Mindist_hist.png",dpi=300)
+    #FIXME add something here to select the CCmin+deltaCC we want to cover a certain percentage of the histogram
     return 0
 
+def get_neighbours(dataset, GPmin=0.25, GPmax=0.35,max_allowed_neighbours=38):
+    neighbours=[]
+    max_neighbours=0
+    for i in range(0,len(dataset['PDB'])):
+        grid=dataset['grid'][i].xyz[0] #set of vectors of dimension 3
+        points_ligand=dataset['points_ligand'][i] # set of integers (indices of gridpoints colliding with ligand atoms)
+        neighbours_i=[] #list of lists that has the neighbours of each grid point that collides with a ligand
+        for j in range(0,len(points_ligand)):
+            neighbours_j_count=0
+            neighbours_j=[]
+            point_lig=grid[j]
+            for k in range(0,len(grid)):
+                point=grid[k]
+                r=np.linalg.norm(point-point_lig)
+                if (GPmin <= r) and (r<=GPmax):
+                   neighbours_j.append(k)
+                   neighbours_j_count += 1
+                   if neighbours_j_count>max_allowed_neighbours:
+                       sys.exit("Too many neighbours detected. You are doing something wrong.")
+                   if neighbours_j_count>max_neighbours:
+                      max_neighbours=neighbours_j_count   
+            neighbours_i.append(neighbours_j)
+        neighbours.append(neighbours_i)
+    print("max neighbours: ", max_neighbours)
+    dataset['Neighbours']=neighbours
+
+    return dataset
+
+def cc2_min_trainer(dataset,theta,percentage):
+    mindist_active=[]
+    for i in range(0,len(dataset['PDB'])):
+        protein=dataset['protein'][i].xyz[0]
+        grid=dataset['grid'][i].xyz[0]
+        neighbours=dataset['Neighbours'][i]
+        for neighbour_list in neighbours:
+            for j in neighbour_list:
+                mindist=calc_mindist(grid[j],protein,theta)
+                mindist_active.append(mindist)
+
+    mindist_active.sort()
+    print("------------ CC2min+deltaCC2 --------------")
+    for pcent in [0.2,0.4,0.6,0.8,1.0]:
+        i=int(len(mindist_active)*pcent)-1
+        cc_i=mindist_active[i]
+        print(pcent, cc_i)
+    pcent=percentage/100
+    i=int(len(mindist_active)*pcent)-1
+    cc=mindist_active[i]
+    print("CC2min+deltaCC2 value that covers ", percentage, "\% of points overlapping with a ligand atom: ", cc)
+    print("------------ CC2min+deltaCC2 --------------")
+    
+    #Plot a histogram for Thesis / paper purposes
+    fig=plt.figure(figsize=(20,10))
+    plt.hist(mindist_active, density=True,stacked=True, bins = 10)
+    plt.xlabel("Minimum distance grid point neighbour - protein atom (nm)",fontsize=20)
+    plt.ylabel("Probability", fontsize=20)
+    plt.savefig("Mindist2_hist.png",dpi=300)
+    return 0
+
+
+
 if __name__=="__main__":
+    # Load structures as mdtraj objects
     args=parser()
     dataset=pd.read_csv(args.input,sep=" ")
     dataset=load_structures(dataset)
-    i=theta_trainer(dataset,r2_target=1.0,tolerance=args.theta_tolerance,max_iter=args.theta_max_iter,d_theta=args.d_theta)
-    print(i)
+
+    # Optimise theta so that the calculated mindist is as well correlated as possible with the actual minimum distance gridpoint-atom
+    #theta=theta_trainer(dataset,r2_target=1.0,tolerance=args.theta_tolerance,max_iter=args.theta_max_iter,d_theta=args.d_theta)
+    
+    # Optimise CCmin+deltaCC so that Son_mind is equal to 1 for the points within 0.2 nm of any ligand heavy atom
+    # 1) Get a list of grid points within 0.2 nm of any ligand heavy atoms.
+    dataset=get_points_ligand(dataset, args.lig_mindist)
+    CC=cc_min_trainer(dataset, 5.0,80.0)
+    dataset=get_neighbours(dataset)
+    CC2=cc2_min_trainer(dataset,5.0,80.0)
+    print(dataset)
+
     
  
