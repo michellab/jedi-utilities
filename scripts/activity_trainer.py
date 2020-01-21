b8d1747e9aec7379efd0cc9ddfca6a046306745c
Joan Clark-Nicolas <joan.clark88@gmail.com>
2020-01-21 15:54:37 +0000

    modified activity_trainer s that it only takes into account ligand atoms within a cutoff (0.6 nm by default)

--- scripts/activity_trainer.py
+++ scripts/activity_trainer.py
@@ -22,6 +22,10 @@ def parser():
     parser.add_argument('-i','--input', nargs="?",
                         help='File containing PDB codes and druggability measures of the trained structures.\
                               Must be in a directory that contains a subdirectory for each PDB.')
+    parser.add_argument('-c','--cutoff', nargs="?",type=str,default=0.6,
+                        help='Maximum distance from the protein for a ligand atom to be considered\
+                              Default will be 0.6.')
+
     args=parser.parse_args()
 
     return args
@@ -40,7 +44,7 @@ def get_atoms(system,lig):
            bsite_indices.append(neighbour)
     return lig_indices, bsite_indices
 
-def get_distances(system,lig_indices,bsite_indices,thetas,pdb):
+def get_distances(system,lig_indices,bsite_indices,thetas,pdb,cutoff):
     minimum_contact=[]
     mind={}
     for theta in thetas:
@@ -56,11 +60,15 @@ def get_distances(system,lig_indices,bsite_indices,thetas,pdb):
         r_min=min(distances[0])
         if (r_min<0.15):
            print("R_min of ",r_min, "found for pdb ",pdb)
+        elif (r_min > cutoff):
+           continue
         minimum_contact.append(min(distances[0]))
 
         for theta in thetas:
             sum_exp=0
             for r in distances[0]:
+                if (r>cutoff):
+                   continue
                 sum_exp += np.exp(theta/r)
             mind_k=theta/np.log(sum_exp)
             mind[theta].append(mind_k)
@@ -79,19 +87,41 @@ def S_off(v,v0,delta):
 
 
 def find_pcents(df,col):
+    pcents=[]
     for pcent in [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]:
         idx=int(len(df[col])*pcent)
         val=sorted(df[col])[idx]
+        pcents.append(val)
         print (pcent, idx, val)
+    return pcents
+
+def get_contacts(system,lig_indices,bsite_indices,v0,delta,cutoff):
+    contacts=[]
+    for lig_idx in lig_indices:
+        depth_j=0
+        atom_pairs=[]
+        for bsite_idx in bsite_indices:
+            if system.topology.atom(bsite_idx).element==mdtraj.element.hydrogen:
+               continue
+            atom_pairs.append([lig_idx,bsite_idx])
+        distances = mdtraj.compute_distances(system, atom_pairs, periodic=True, opt=True)
+        for distance in distances[0]:
+            if (distance > cutoff):
+               continue
+            depth_j+=S_off(distance,v0,delta)
+        contacts.append(depth_j)
+
+    return contacts
 
 if __name__=="__main__":
     # Load structures as mdtraj objects
     args=parser()
     dataset=pd.read_csv(args.input,sep=" ")
+    cutoff=args.cutoff
 
     r_min=[]
 
-    thetas=[1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,50]
+    thetas=xrange(1,101)
     mindist={}
     for theta in thetas:
         mindist[theta]=[]
@@ -100,10 +130,11 @@ if __name__=="__main__":
         system=mdtraj.load(dataset["PDB"][i])
         lig_indices, bsite_indices=get_atoms(system,dataset["LIGAND"][i])
         
-        minimum_contact, mind=get_distances(system,lig_indices,bsite_indices,thetas,dataset["PDB"][i])
+        minimum_contact, mind=get_distances(system,lig_indices,bsite_indices,thetas,dataset["PDB"][i],cutoff)
         r_min+=minimum_contact
         for theta in thetas:
             mindist[theta]+=mind[theta]
+   
 
     dists=pd.DataFrame()
     dists["r_min"]=r_min
@@ -120,6 +151,21 @@ if __name__=="__main__":
 
     find_pcents(dists,'r_min')
     find_pcents(dists,'50')
-     
+         
+    pcents=find_pcents(dists,'r_min')
+    rmin_max=max(dists['r_min'])
+    contacts_pcents=pd.DataFrame()
+    for val in pcents:
+        contacts=[]
+        r_hydro=val
+        deltar_hydro=rmin_max-val
+        for i in xrange(0,len(dataset)):
+            system=mdtraj.load(dataset["PDB"][i])
+            lig_indices, bsite_indices=get_atoms(system,dataset["LIGAND"][i])
+            contacts+=get_contacts(system,lig_indices,bsite_indices,r_hydro,deltar_hydro,cutoff)
+        label="r_hydro="+str(r_hydro)+" deltar="+str(deltar_hydro)
+        contacts_pcents[label]=contacts
+ 
     dists.to_pickle("dists.pkl")
     corr.to_pickle("corr.pkl")
+    contacts_pcents.to_pickle("contacts.pkl")
